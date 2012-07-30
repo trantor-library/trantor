b70ddd81ab8b08bd9f0e29653976c6ab43f02abf
Las Zenow <zenow@tormail.org>
2012-07-30 23:59:55 +0200

    10 items per page on search

--- search.go
+++ search.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"strconv"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 	"net/http"
 	"strings"
+)
+
+const (
+	ITEMS_PAGE = 10
 )
 
 func buildQuery(q string) bson.M {
@@ -19,7 +24,11 @@ func buildQuery(q string) bson.M {
 
 type searchData struct {
 	Search string
+	Found  int
 	Books  []Book
+	Page int
+	Next string
+	Prev string
 }
 
 func searchHandler(coll *mgo.Collection, w http.ResponseWriter, r *http.Request) {
@@ -31,5 +40,29 @@ func searchHandler(coll *mgo.Collection, w http.ResponseWriter, r *http.Request)
 	req := strings.Join(r.Form["q"], " ")
 	var res []Book
 	coll.Find(buildQuery(req)).All(&res)
-	loadTemplate(w, "search", searchData{req, res})
+
+	page := 0
+	if len(r.Form["p"]) != 0 {
+		page, err = strconv.Atoi(r.Form["p"][0])
+		if err != nil || len(res) < ITEMS_PAGE*page {
+			page = 0
+		}
+	}
+
+	var data searchData
+	data.Search = req
+	data.Found = len(res)
+	if len(res) > ITEMS_PAGE*(page+1) {
+		data.Books = res[ITEMS_PAGE*page:ITEMS_PAGE*(page+1)]
+	} else {
+		data.Books = res[ITEMS_PAGE*page:]
+	}
+	data.Page = page+1
+	if len(res) > (page+1)*ITEMS_PAGE {
+		data.Next = "/search/?q=" + req + "&p=" + strconv.Itoa(page+1)
+	}
+	if page > 0 {
+		data.Prev = "/search/?q=" + req + "&p=" + strconv.Itoa(page-1)
+	}
+	loadTemplate(w, "search", data)
 }
