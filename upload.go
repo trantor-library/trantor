beb032eddb63b54cbbf2e2f03040d8f40455bedb
Las Zenow <zenow@tormail.org>
2012-08-15 12:29:54 +0200

    Check if the filename exists

--- upload.go
+++ upload.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"os"
+	"strconv"
 	"labix.org/v2/mgo"
 	//"labix.org/v2/mgo/bson"
 	"net/http"
 )
+
+func storePath(name string) string {
+	path := "new/" + name
+	_, err := os.Stat(path)
+	for i := 0; err == nil; i++ {
+		path = "new/" + strconv.Itoa(i) + "_" + name
+		_, err = os.Stat(path)
+	}
+	return path
+}
 
 func storeFile(r *http.Request) error {
 	f, header, err := r.FormFile("epub")
@@ -13,8 +24,8 @@ func storeFile(r *http.Request) error {
 		return err
 	}
 	defer f.Close()
-	// FIXME: check the name exist
-	fw, err := os.Create("new/" + header.Filename)
+
+	fw, err := os.Create(storePath(header.Filename))
 	if err != nil {
 		return err
 	}
