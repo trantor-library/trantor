d924b577d1d5bbaebeb2ddcc87d06ea99be79d3d
Las Zenow <zenow@tormail.org>
2012-08-15 15:11:52 +0200

    Move templates

--- template.go
+++ template.go
@@ -5,9 +5,19 @@ import (
 	"net/http"
 )
 
+const (
+	TEMPLATE_DIR = "templates/"
+)
+
 func loadTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	// TODO: when finish devel conver to global:
-	var templates = template.Must(template.ParseFiles("header.html", "footer.html", "index.html", "about.html", "book.html", "search.html", "upload.html"))
+	var templates = template.Must(template.ParseFiles(TEMPLATE_DIR + "header.html",
+							  TEMPLATE_DIR + "footer.html",
+							  TEMPLATE_DIR + "index.html",
+							  TEMPLATE_DIR + "about.html",
+							  TEMPLATE_DIR + "book.html",
+							  TEMPLATE_DIR + "search.html",
+							  TEMPLATE_DIR + "upload.html"))
 
 	err := templates.ExecuteTemplate(w, tmpl+".html", data)
 	if err != nil {
