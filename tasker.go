725b1d89518f9b3b050ca8cfdc5170e687d959e8
Las Zenow <zenow@riseup.net>
2014-02-11 11:12:36 +0100

    Set different times for update stadistics of visits and downloads

--- tasker.go
+++ tasker.go
@@ -8,12 +8,12 @@ func InitTasks(db *DB) {
 	periodicTask(db.UpdateTags, MINUTES_UPDATE_TAGS*time.Minute)
 	periodicTask(db.UpdateMostVisited, MINUTES_UPDATE_VISITED*time.Minute)
 	periodicTask(db.UpdateDownloadedBooks, MINUTES_UPDATE_DOWNLOADED*time.Minute)
-	periodicTask(db.UpdateHourVisits, MINUTES_UPDATE_HOURLY*time.Minute)
-	periodicTask(db.UpdateDayVisits, MINUTES_UPDATE_DAILY*time.Minute)
-	periodicTask(db.UpdateMonthVisits, MINUTES_UPDATE_MONTHLY*time.Minute)
-	periodicTask(db.UpdateHourDownloads, MINUTES_UPDATE_HOURLY*time.Minute)
-	periodicTask(db.UpdateDayDownloads, MINUTES_UPDATE_DAILY*time.Minute)
-	periodicTask(db.UpdateMonthDownloads, MINUTES_UPDATE_MONTHLY*time.Minute)
+	periodicTask(db.UpdateHourVisits, MINUTES_UPDATE_HOURLY_V*time.Minute)
+	periodicTask(db.UpdateDayVisits, MINUTES_UPDATE_DAILY_V*time.Minute)
+	periodicTask(db.UpdateMonthVisits, MINUTES_UPDATE_MONTHLY_V*time.Minute)
+	periodicTask(db.UpdateHourDownloads, MINUTES_UPDATE_HOURLY_D*time.Minute)
+	periodicTask(db.UpdateDayDownloads, MINUTES_UPDATE_DAILY_D*time.Minute)
+	periodicTask(db.UpdateMonthDownloads, MINUTES_UPDATE_MONTHLY_D*time.Minute)
 }
 
 func periodicTask(task func() error, periodicity time.Duration) {
